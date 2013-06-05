932d8cf70b0fbc53de5ac561cbd618de8eba90c0
martincronje <martin@nreality.com>
2013-06-05 22:19:02 +0200

    changes to start automating setup

---
+++ com.apple.settings.osx.sh
@@ -0,0 +1,3 @@
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool YES
+defaults write com.apple.screensaver askForPassword -int 1
+defaults write com.apple.screensaver askForPasswordDelay -int 600  
