d019542a29ba2ecd8681b3403a956261420ab5c6
Naudé Cruywagen <naude@glucode.com>
2018-02-09 11:42:58 +0200

    Added cloning of dotfiles repo to setup script

--- setup-new-mac.sh
+++ setup-new-mac.sh
@@ -1,44 +1,26 @@
 #/bin/bash
 
-
-# this is a work in progress. I will probably only finish it if I ever need to rebuild an OSX instance from scratch.
+# this is a work in progress. 
 
 echo "Instaling Homebrew"
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
-ruby -e "$(curl -fsSL https://raw.github.com/mxcl/homebrew/go)"
-
+echo "Instaling macvim"
 brew install macvim
-brew install git
 
+echo "Setting PATH"
 export PATH=/usr/local/bin:$PATH
 
-git config --global user.name "Martin Cronjé"
-git config --global user.email "martin@nreality.com"
+echo "Creating git directory"
+mkdir ~/git
 
-curl -s -O \
-  http://github-media-downloads.s3.amazonaws.com/osx/git-credential-osxkeychain
-
-chmod u+x git-credential-osxkeychain
-
-sudo mv git-cedential-osxkeychain `dirname \`which git\``
-
-git config --global credential.helper osxkeychain
-
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
-
-git clone https://github.com/martincronje/dotfiles ~/.amok-osx-config
+echo "Cloning dotfiles repo"
+git clone git@github.com:naudecruywagen/dotfiles.git ~/git
 
 echo "Linking dotfiles"
+ln -s ~/git/dotfiles/.zshrc ~/.zshrc
+ln -s ~/git/dotfiles/.ideavimrc ~/.ideavimrc
+ln -s ~/git/dotfiles/.xvimrc ~/.xvimrc
 
-ln -s ~/.dotfiles/config/.tmux.comf ~/.tmux.conf
-ln -s ~/.dotfiles/config/.tmuxinator ~/.tmuxinator
-ln -s ~/.dotfiles/config/.vimrc ~/.vimrc
-ln -s ~/.dotfiles/config/.gvimrc ~/.gvimrc
-
-ln -s ~/.dotfiles/config/zsh/themes/amok.zsh-theme  ~/.oh-my-zsh/themes/amok.zsh-themeD
-ln -s ~/.dotfiles/config/.vim/colors/Monokai.vim ~/.vim/colors/Monokai.vim
-ln -s ~/.dotfiles/config/.gitconfig ~/.gitconfig
-
-echo "RVM"
-
-curl -#L https://get.rvm.io | bash -s stable --autolibs=3 --ruby
+echo "Instaling oh-my-zsh"
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
